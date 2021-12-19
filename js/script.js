594494fc4c8bf80cf8e9dee7884c37216b616c3e
lenasorken <lena_sorken@hotmail.com>
2021-12-19 23:55:45 +0100

    .

--- js/script.js
+++
@@ -1,9 +0,0 @@
-var removeCartItemButtons = document.getElementsByClassName('button')
-console.log()
-
-for (var i = 0 < removeCartItemButtons.length; i++) {
-    var button = removeCartItemButtons[i]
-    button.addEventListener('click', funtion(event) {
-        console.log()
-    
-
