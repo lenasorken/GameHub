16e91ea043e7f76f909735e64467b0478b5115b4
lenasorken <lena_sorken@hotmail.com>
2022-01-23 23:57:28 +0100

    api call

---
+++ js/api.js
@@ -0,0 +1,25 @@
+const url = "http://lfs.one/gamehub/wp-json/wc/store/products";
+const productsContainer = document.querySelector(".games_page_container");
+
+
+async function getProducts() {
+    try{
+        const response = await fetch (url);
+        const getResults = await response.json();
+        createHTML(getResults);
+
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+getProducts();
+
+function createHTML(products) {
+    products.forEach(function(product) {
+        productsContainer.innerHTML += '<div class="game_list">' + product.name + "</div>";
+    })
+}
+
+
